ebfabbbb939c5d5974a227440228ea203e7de909
syndrome <yaxu@thoughtworks.com>
2019-03-12 16:25:06 +0800

    [syndrome] #3 for dead lock

--- src/main/java/com/thoughtworks/concurrent/dojo/z1/DeadLock.java
+++ src/main/java/com/thoughtworks/concurrent/dojo/z1/DeadLock.java
@@ -10,37 +10,37 @@ public class DeadLock {
     private static void aToB() {
         synchronized (a) {
             System.out.println("a to b -- a start");
-        }
 
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {
+                TimeUnit.SECONDS.sleep(12);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        synchronized (b) {
-            System.out.println("a to b -- b start");
+            synchronized (b) {
+                System.out.println("a to b -- b start");
+            }
         }
     }
 
     private static void bToA() {
         synchronized (b) {
             System.out.println("b to a -- b start");
-        }
 
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            try {
+                TimeUnit.SECONDS.sleep(12);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        synchronized (a) {
-            System.out.println("b to a -- a start");
+            synchronized (a) {
+                System.out.println("b to a -- a start");
+            }
         }
     }
 
     public static void main(String[] args) {
-        new Thread(() -> aToB()).start();
-        new Thread(() -> bToA()).start();
+        new Thread(DeadLock::aToB).start();
+        new Thread(DeadLock::bToA).start();
     }
 }
